b520cf764d68f3e47200f63f31a813122ef687ad
Sean Aye <hello@seanaye.ca>
2020-10-27 13:44:30 -0400

    add vue composition api

--- pages/users.vue
+++ pages/users.vue
@@ -10,9 +10,26 @@
                 v-list-item-avatar
                   img(:src='require("@/assets/img/user_avatar.png")')
                 v-list-item-content
-                  | John Smith
+                  | {{ name }}
               v-container
                 v-row
                   v-col(cols='12' md='6' lg='4' xl='3')
-                    img(:src='require("@/assets/img/waterfall.jpeg")' style='object-fit: contain; width: 100%;')
+                    img(
+                      :src='require("@/assets/img/waterfall.jpeg")'
+                      style='object-fit: contain; width: 100%;'
+                    )
 </template>
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  setup () {
+    // example setup function feel free to delete
+    const name = ref('John Smith')
+
+    return {
+      name
+    }
+  }
+})
+</script>
