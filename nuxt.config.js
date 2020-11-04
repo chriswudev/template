fd19e84d89212ed6920fa458dc4cb32953aec1c1
chriswudev <chris.wu.tiff@gmail.com>
2020-11-04 09:44:07 -0500

    remove axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,15 +40,11 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     '@nuxtjs/apollo'
   ],
   apollo: {
     clientConfigs: {}
   },
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {},
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
